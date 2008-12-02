a65d87e5b1ff395630888394a6208f0466125731
SZEDER Gábor <szeder@fzi.de>
2008-12-02 02:26:09 +0100

    Helper class for system-specific line separator
    '\n' is not portable.
    
    Signed-off-by: SZEDER Gábor <szeder@fzi.de>

---
+++ src/main/de/fzi/cjunit/util/LineSeparator.java
@@ -0,0 +1,15 @@
+/*
+ * This file is covered by the terms of the Common Public License v1.0.
+ *
+ * Copyright (c) SZEDER Gábor
+ *
+ * Parts of this software were developed within the JEOPARD research
+ * project, which received funding from the European Union's Seventh
+ * Framework Programme under grant agreement No. 216682.
+ */
+
+package de.fzi.cjunit.util;
+
+public class LineSeparator {
+	public static String lineSeparator = System.getProperty("line.separator");
+}
