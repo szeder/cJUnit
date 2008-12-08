d8ada58e13783c9a98f01b919f1e0552d8424546
SZEDER Gábor <szeder@fzi.de>
2008-12-08 13:37:38 +0100

    TestWrapper class with basic functionality
    JPF can run only whole applications, IOW it requires a main() method.
    The TestWrapper class is used to wrap up individual test methods in a
    program, so we can run a test method under JPF.  In the future it will
    be extended to handle expected exceptions and invoke @Before methods,
    too.
    
    The name of the test class and the test method are supplied on the
    command line.  After parsing the command line arguments the test class
    is instantiated and the test method is called.
    
    Signed-off-by: SZEDER Gábor <szeder@fzi.de>

---
+++ src/main/de/fzi/cjunit/jpf/inside/TestWrapper.java
@@ -0,0 +1,83 @@
+/*
+ * This file is covered by the terms of the Common Public License v1.0.
+ *
+ * Copyright (c) SZEDER Gábor
+ *
+ * Parts of this software were developed within the JEOPARD research
+ * project, which received funding from the European Union's Seventh
+ * Framework Programme under grant agreement No. 216682.
+ */
+
+package de.fzi.cjunit.jpf.inside;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TestWrapper {
+
+	String testClassName;
+	String testMethodName;
+
+	Object target;
+	Method method;
+
+	public TestWrapper(String... args) {
+		parseArgs(args);
+	}
+
+	public void parseArgs(String... args) {
+		for (String arg : args) {
+			if (arg.startsWith("--testclass=")) {
+				testClassName = arg.substring(
+						arg.indexOf('=')+1);
+			} else if (arg.startsWith("--testmethod=")) {
+				testMethodName = arg.substring(
+						arg.indexOf('=')+1);
+			} else {
+				throw new RuntimeException("wrong command " +
+						"line parameter: " + arg);
+			}
+		}
+	}
+
+	public void run() throws IllegalArgumentException, SecurityException,
+			InstantiationException, IllegalAccessException,
+			InvocationTargetException, NoSuchMethodException,
+			ClassNotFoundException {
+		// The testing framework calling this has already checked
+		// that both the class and the method exists
+		createTestObject();
+		createTestMethod();
+
+		runTest();
+	}
+
+	public void runTest() throws IllegalArgumentException,
+			IllegalAccessException, InvocationTargetException {
+		method.invoke(target);
+	}
+
+	public void createTestObject() throws
+			IllegalArgumentException, SecurityException,
+			InstantiationException, IllegalAccessException,
+			InvocationTargetException, NoSuchMethodException,
+			ClassNotFoundException {
+		target = Class.forName(testClassName)
+				.getConstructor()
+				.newInstance();
+	}
+
+	public void createTestMethod() throws SecurityException,
+			NoSuchMethodException {
+		method = createMethod(testMethodName);
+	}
+
+	public Method createMethod(String methodName) throws SecurityException,
+			NoSuchMethodException {
+		return target.getClass().getMethod(methodName);
+	}
+
+	public static void main(String... args) throws Throwable {
+		new TestWrapper(args).run();
+	}
+}
--- src/test/de/fzi/cjunit/BaseTests.java
+++ src/test/de/fzi/cjunit/BaseTests.java
@@ -21,7 +21,8 @@ import org.junit.runners.Suite.SuiteClasses;
 	de.fzi.cjunit.runners.ConcurrentRunnerTest.class,
 	de.fzi.cjunit.jpf.util.ArgumentCreatorTest.class,
 	de.fzi.cjunit.jpf.util.StackFrameConverterTest.class,
-	de.fzi.cjunit.jpf.util.ExceptionFactoryTest.class
+	de.fzi.cjunit.jpf.util.ExceptionFactoryTest.class,
+	de.fzi.cjunit.jpf.inside.TestWrapperTest.class
 })
 public class BaseTests {
 }
