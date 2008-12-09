4bca170a083ec2688dfb58cf27691178ca9de405
SZEDER Gábor <szeder@fzi.de>
2008-12-09 14:33:27 +0100

    Add TestObserver property listener class
    To find out whether a test run under JPF succeeded or failed, and how
    it failed, the test run must be observed.  There are two things that
    are interesting for us:
    
     * Calls to notifier methods:  if a test fails, the TestWrapper will
       call NotifierMethods.testFailed().
       Therefore, TestObserver overrides executeInstruction() from
       VMListener, and checks whether the instruction is an invoke
       instruction, and if it is, then whether it invokes
       NotifierMethods.testFailed().
     * Exceptions thrown:  a test method usually fails with an exception.
       This exception must be "forwarded" from within JPF to the unit
       testing framework invoking JPF.
       Therefore, TestObserver overrides exceptionThrown() from
       VMListener, and stores stores the information about the exception.
       This infomation will be used later to reconstruct that exception
       and throw it up to the unit testing framework.
    
    Signed-off-by: SZEDER Gábor <szeder@fzi.de>

---
+++ src/main/de/fzi/cjunit/jpf/outside/TestObserver.java
@@ -0,0 +1,123 @@
+/*
+ * This file is covered by the terms of the Common Public License v1.0.
+ *
+ * Copyright (c) SZEDER Gábor
+ *
+ * Parts of this software were developed within the JEOPARD research
+ * project, which received funding from the European Union's Seventh
+ * Framework Programme under grant agreement No. 216682.
+ */
+
+package de.fzi.cjunit.jpf.outside;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Stack;
+
+import gov.nasa.jpf.PropertyListenerAdapter;
+import gov.nasa.jpf.jvm.ExceptionInfo;
+import gov.nasa.jpf.jvm.JVM;
+import gov.nasa.jpf.jvm.MethodInfo;
+import gov.nasa.jpf.jvm.StackFrame;
+import gov.nasa.jpf.jvm.ThreadInfo;
+import gov.nasa.jpf.jvm.bytecode.Instruction;
+import gov.nasa.jpf.jvm.bytecode.InvokeInstruction;
+import gov.nasa.jpf.search.Search;
+
+import de.fzi.cjunit.jpf.inside.NotifierMethods;
+import de.fzi.cjunit.jpf.util.ExceptionFactory;
+import de.fzi.cjunit.jpf.util.StackFrameConverter;
+
+
+public class TestObserver extends PropertyListenerAdapter {
+
+	boolean result = true;
+	boolean testSucceeded = true;
+
+	String exceptionClassName;
+	String exceptionMessage;
+	StackFrame[] stackTrace;
+
+	Stack<Boolean> stateStack = new Stack<Boolean>();
+
+	public boolean getTestResult() {
+		return testSucceeded;
+	}
+
+	public Throwable getException() throws IllegalArgumentException,
+			SecurityException, InstantiationException,
+			IllegalAccessException, InvocationTargetException,
+			ClassNotFoundException {
+		return ExceptionFactory.createException(exceptionClassName,
+				exceptionMessage,
+				StackFrameConverter.toStackTrace(stackTrace));
+	}
+
+	public void testFailed() {
+		result = false;
+		testSucceeded = false;
+	};
+
+	// from Property
+	@Override
+	public boolean check(Search search, JVM jvm) {
+		return result;
+	}
+
+	@Override
+	public String getErrorMessage() {
+		return "test failed";
+	}
+
+	// from VMListener
+	@Override
+	public void exceptionThrown(JVM vm) {
+		ThreadInfo ti = vm.getLastThreadInfo();
+		ExceptionInfo ei = vm.getPendingException();
+
+		if (testSucceeded) {
+			exceptionClassName = ei.getExceptionClassname();
+			String exceptionDetails = ei.getDetails();
+			// It seems that the string returned by
+			// ExceptionInfo.getDetails() looks like
+			// "<exception type> : <message>".  To get the original
+			// message we must strip the type prefix.
+			if (exceptionDetails.startsWith(
+					exceptionClassName + " : ")) {
+				exceptionMessage = exceptionDetails.substring(
+						exceptionClassName.length()+3);
+			} else {
+				exceptionMessage = exceptionDetails;
+			}
+			stackTrace = ti.dumpStack();
+		}
+	}
+
+	@Override
+	public void executeInstruction(JVM vm) {
+		Instruction insn = vm.getLastInstruction();
+
+		if (insn instanceof InvokeInstruction) {
+			InvokeInstruction callInsn = (InvokeInstruction) insn;
+			ThreadInfo ti = vm.getLastThreadInfo();
+
+			MethodInfo callee = callInsn.getInvokedMethod(ti);
+			if (callee.getClassName().equals(
+					NotifierMethods.class.getName())) {
+				if (callee.getName().equals("testFailed")) {
+					testFailed();
+				}
+			}
+		}
+	}
+
+	// from SearchListener
+	@Override
+	public void stateAdvanced(Search search) {
+		stateStack.push(result);
+	}
+
+	@Override
+	public void stateBacktracked(Search search) {
+		result = stateStack.pop();
+	}
+}
---
+++ src/test/de/fzi/cjunit/jpf/outside/TestObserverTest.java
@@ -0,0 +1,93 @@
+/*
+ * This file is covered by the terms of the Common Public License v1.0.
+ *
+ * Copyright (c) SZEDER Gábor
+ *
+ * Parts of this software were developed within the JEOPARD research
+ * project, which received funding from the European Union's Seventh
+ * Framework Programme under grant agreement No. 216682.
+ */
+
+package de.fzi.cjunit.jpf.outside;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+import gov.nasa.jpf.Config;
+import gov.nasa.jpf.JPF;
+import gov.nasa.jpf.PropertyListenerAdapter;
+
+import static de.fzi.cjunit.jpf.inside.NotifierMethods.*;
+import de.fzi.cjunit.jpf.util.ArgumentCreator;
+
+
+public class TestObserverTest {
+
+	void createAndRunJPF(Class<?> appClass,
+			PropertyListenerAdapter propertyListener) {
+		String[] jpfArgs = new ArgumentCreator()
+				.app(appClass)
+				.defaultJPFTestArgs()
+				.getArgs();
+
+		Config conf = JPF.createConfig(jpfArgs);
+		JPF jpf = new JPF(conf);
+		jpf.addPropertyListener(propertyListener);
+		jpf.run();
+	}
+
+	public static class SucceedingTestClass {
+		public static void main(String... args) {
+		}
+	}
+
+	@Test
+	public void succeedingTest() {
+		TestObserver to = new TestObserver();
+		createAndRunJPF(SucceedingTestClass.class, to);
+
+		assertThat(to.getTestResult(), equalTo(true));
+	}
+
+	public static class FailingTestClass {
+		public static void main(String... args) {
+			testFailed();
+		}
+	}
+
+	@Test
+	public void failingTest() {
+		TestObserver to = new TestObserver();
+		createAndRunJPF(FailingTestClass.class, to);
+
+		assertThat(to.getTestResult(), equalTo(false));
+	}
+
+	public static class ExceptionThrowingClass {
+		public static void main(String... args) {
+			throw new RuntimeException("asdf");
+		}
+	}
+
+	@Test
+	public void exceptionInfo() {
+		TestObserver to = new TestObserver();
+		createAndRunJPF(ExceptionThrowingClass.class, to);
+
+		assertThat(to.exceptionClassName, equalTo(
+				"java.lang.RuntimeException"));
+		assertThat(to.exceptionMessage, equalTo("asdf"));
+	}
+
+	@Test
+	public void getException() throws Throwable {
+		TestObserver to = new TestObserver();
+		createAndRunJPF(ExceptionThrowingClass.class, to);
+
+		Throwable t = to.getException();
+		assertThat(t, is(RuntimeException.class));
+		assertThat(t.getMessage(), equalTo("asdf"));
+	}
+}
