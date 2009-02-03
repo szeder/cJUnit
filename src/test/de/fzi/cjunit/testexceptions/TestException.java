b16b7899cc4516c208aa29141177ac588a04259a
SZEDER Gábor <szeder@fzi.de>
2009-02-03 18:25:38 +0100

    Add various exception classes to help testing
    There are already some tests dealing with exceptions, and there are
    even more such tests coming up.  To avoid the need for these test
    classes to declare their own exceptions, here we add some common ones.
    
    Signed-off-by: SZEDER Gábor <szeder@fzi.de>

---
+++ src/test/de/fzi/cjunit/testexceptions/TestException.java
@@ -0,0 +1,30 @@
+/*
+ * This file is covered by the terms of the Common Public License v1.0.
+ *
+ * Copyright (c) SZEDER Gábor
+ *
+ * Parts of this software were developed within the JEOPARD research
+ * project, which received funding from the European Union's Seventh
+ * Framework Programme under grant agreement No. 216682.
+ */
+
+package de.fzi.cjunit.testexceptions;
+
+public class TestException extends Throwable {
+
+	private static final long serialVersionUID = 1L;
+
+	public TestException() { }
+
+	public TestException(String msg) {
+		super(msg);
+	}
+
+	public TestException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public TestException(Throwable t) {
+		super(t);
+	}
+}
