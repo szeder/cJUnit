d6935dabf851f61a8813e3c4d9b39c75f136d088
SZEDER Gábor <szeder@fzi.de>
2009-09-06 23:18:06 +0200

    AssumptionsAboutJPF: exceptions in threads cause property violation
    Signed-off-by: SZEDER Gábor <szeder@fzi.de>

--- src/test/de/fzi/cjunit/jpf/outside/AssumptionsAboutJPF.java
+++ src/test/de/fzi/cjunit/jpf/outside/AssumptionsAboutJPF.java
@@ -13,12 +13,17 @@ package de.fzi.cjunit.jpf.outside;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import gov.nasa.jpf.Config;
+import gov.nasa.jpf.Error;
 import gov.nasa.jpf.GenericProperty;
 import gov.nasa.jpf.JPF;
+import gov.nasa.jpf.Property;
 import gov.nasa.jpf.jvm.JVM;
+import gov.nasa.jpf.jvm.NoUncaughtExceptionsProperty;
 import gov.nasa.jpf.search.Search;
 
 import de.fzi.cjunit.jpf.util.ArgumentCreator;
@@ -59,5 +64,29 @@ public class AssumptionsAboutJPF {
 
 		assertThat("only one property violation is reported",
 				jpf.getSearchErrors().size(), equalTo(1));
+	}
+
+	public static class ThrowInThread {
+		public static void main(String... args) throws Throwable {
+			Thread t = new Thread() {
+				@Override
+				public void run() {
+					throw new AssertionError();
+				}
+			};
+			t.start();
+			t.join();
+		}
+	}
+	@Test
+	public void exceptionInThreadCausesPropertyViolation() {
+		createJPF(ThrowInThread.class);
+		Property property = new NoUncaughtExceptionsProperty(config);
+		jpf.addSearchProperty(property);
+		jpf.run();
+
+		List<Error> errors = jpf.getSearchErrors();
+		assertThat("there is an error", errors.size(), equalTo(1));
+		assertThat(errors.get(0).getProperty(), equalTo(property));
 	}
 }
