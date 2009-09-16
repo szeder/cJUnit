0558d1526a9dfc4b5d9fa49e4504fd779eed78fb
SZEDER Gábor <szeder@fzi.de>
2009-09-16 19:05:28 +0200

    TestWrapperTest: use Counter class instead of boolean member variable
    This way we can avoid the member variable in the test class which is only
    related to a single test case.
    
    Signed-off-by: SZEDER Gábor <szeder@fzi.de>

--- src/test/de/fzi/cjunit/jpf/inside/TestWrapperTest.java
+++ src/test/de/fzi/cjunit/jpf/inside/TestWrapperTest.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import de.fzi.cjunit.testexceptions.*;
+import de.fzi.cjunit.testutils.Counter;
 
 
 public class TestWrapperTest {
@@ -256,17 +257,13 @@ public class TestWrapperTest {
 		tw.runBeforeMethods();
 	}
 
-	// Can't put it inside the method, because it has to be final to be
-	// accessible from the inner class, but if it's final, then it can't be
-	// modified.  Catch-22.
-	boolean testMethodInvoked = false;
-
 	@Test
 	public void testMethodIsNotInvokedIfABeforeMethodFails()
 			throws Throwable {
+		final Counter invocationCounter = new Counter();
 		TestWrapper tw = new TestWrapper() {
 			protected void invokeTestMethod() {
-				testMethodInvoked = true;
+				invocationCounter.increment();
 			}
 		};
 		tw.target = this;
@@ -277,7 +274,8 @@ public class TestWrapperTest {
 			tw.runTest();
 		} catch (TestException te) {}
 
-		assertThat(testMethodInvoked, equalTo(false));
+		assertThat("not invoked", invocationCounter.getValue(),
+				equalTo(0));
 	}
 
 	@Test
