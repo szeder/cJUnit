bc02ef447195c6fb3b444778029121674c5278b0
SZEDER Gábor <szeder@fzi.de>
2010-01-13 17:06:02 +0100

    Add integration tests using the threadGroup annotation parameter
    Signed-off-by: SZEDER Gábor <szeder@fzi.de>

--- src/test/de/fzi/cjunit/integration/testclasses/SuccessfulTests.java
+++ src/test/de/fzi/cjunit/integration/testclasses/SuccessfulTests.java
@@ -23,6 +23,9 @@ import de.fzi.cjunit.util.TestBarrier;
 
 public class SuccessfulTests {
 
+	final int TGID_Basic = 1;
+	final int TGID_WithException = 2;
+
 	public static boolean invoked = false;
 	Counter counter = new Counter();
 
@@ -67,6 +70,32 @@ public class SuccessfulTests {
 
 	@ConcurrentTest(threadCount=2,expected=TestException.class)
 	public void concurrentTestWithThreadCountAndException()
+			throws Throwable {
+		throw new TestException();
+	}
+
+	@ConcurrentTest(threadGroup=TGID_Basic)
+	public void concurrentTestWithThreadGroup1() {
+		TestBarrier.await();
+		assertThat(counter.getValue(), equalTo(1));
+	}
+
+	@ConcurrentTest(threadGroup=TGID_Basic)
+	public void concurrentTestWithThreadGroup2() {
+		synchronized (this) {
+			counter.increment();
+		}
+		TestBarrier.await();
+	}
+
+	@ConcurrentTest(threadGroup=TGID_WithException)
+	public void concurrentTestWithThreadGroupAndException1() {
+		assertThat(true, equalTo(true));
+	}
+
+	@ConcurrentTest(threadGroup=TGID_WithException,
+			expected=TestException.class)
+	public void concurrentTestWithThreadGroupAndException2()
 			throws Throwable {
 		throw new TestException();
 	}
